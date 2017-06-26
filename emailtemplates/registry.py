8aa2485a088f00f4237c6c13709dd13489d18a49
Piotr Roksela <piotr.roksela@deployed.pl>
2017-06-26 18:04:45 +0200

    registration item class

--- emailtemplates/registry.py
+++ emailtemplates/registry.py
@@ -13,6 +13,14 @@ class NotRegistered(Exception):
     pass
 
 
+class RegistrationItem(object):
+
+    def __init__(self, path, help_text=None, help_context=None):
+        self.path = path
+        self.help_text = help_text or u""
+        self.help_context = help_context or {}
+
+
 class EmailTemplateRegistry(object):
 
     def __init__(self):
@@ -34,30 +42,36 @@ class EmailTemplateRegistry(object):
         """
         if path in self._registry:
             raise AlreadyRegistered('The template %s is already registered' % path)
-        self._registry[path] = (help_text, help_context)
+        self._registry[path] = RegistrationItem(path, help_text, help_context)
         logger.debug("Registered email template %s", path)
 
+    def is_registered(self, path):
+        return path in self._registry
+
     def _get_registration(self, path):
-        if path not in self._registry:
+        if not self.is_registered(path):
             raise NotRegistered("Email template not registered")
         return self._registry[path]
 
     def get_help_text(self, path):
-        return self._get_registration(path)[0]
+        return self._get_registration(path).help_text
 
     def get_help_context(self, path):
-        return self._get_registration(path)[1]
+        return self._get_registration(path).help_context
 
     def get_email_template_choices(self):
         return [(template_name, template_name) for template_name in list(set(self._registry.keys()))]
 
     def get_context_description(self, path):
-        # return u", ".join([u"'%s' (%s)" % (k, v) for k, v in self.get_help_context(path)])
         help_context = self.get_help_context(path) or {}
         return u", ".join([u"%s (%s)" % (k, v) for (k, v) in help_context.items()])
 
     def get_admin_help_text_information(self, path):
         return u"USAGE: %s \nCONTEXT: %s" % (self.get_help_text(path), self.get_context_description(path))
+
+    def create_eft(self, path, *args, **kwargs):
+        if self._get_registration(path):
+            return
 
 
 # Global object for singleton registry of email templates
