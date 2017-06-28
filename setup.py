7c03802a688063b1bbbdccddd9202b7803b2fce6
Piotr Roksela <piotr.roksela@deployed.pl>
2017-06-28 15:23:57 +0200

    authors and version in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='django-emailtemplates',
-    version='1.0.0-rc.2',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
