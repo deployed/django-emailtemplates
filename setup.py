df45da90211c9bb4e8af90d859a002632d0f0eae
Michał <czubik1@vp.pl>
2021-02-15 14:10:20 +0100

    Bumped version to 1.1.10

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='django-emailtemplates',
-    version='1.1.9',
+    version='1.1.10',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
